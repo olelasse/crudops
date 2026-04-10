218429dcb6e7081e025e434c3bfb538d3ab8f220
Ole Lasse Bjellaas <olelasse@gmail.com>
2026-04-10 21:34:53 +0200

    jobber med en login hook

--- index.js
+++ index.js
@@ -88,24 +88,33 @@ server.post("/api/reset", (request, response) => {
 
 /* Endepunkt: Login med e-post og passord. Returnerer API_KEY ved gyldig innlogging. */
 server.post("/api/login", (request, response) => {
-  const { email, password } = request.body || {};
+  const { email, password, usertype } = request.body || {};
   const identifier = email;
 
   if (!identifier || !password) {
-    response.status(400).json({ message: "Mangler e-post og/eller passord." });
+    response
+      .status(400)
+      .json({ message: "Mangler e-post og/eller passord.", type: "general" });
     return;
   }
 
+  // Må legge inn en sjekk her for usertype. og legge til riktig routing!
   const users = router.db.get("users").value();
-  const user = users.find(
-    (u) => u.email === identifier && u.password === password,
-  );
+  const user = users.find((u) => u.email === identifier);
 
-  if (user) {
-    response.status(200).json({ API_KEY: process.env.API_KEY });
-  } else {
-    response.status(401).json({ message: "Ugyldig brukernavn eller passord." });
+  if (!user) {
+    response
+      .status(401)
+      .json({ message: "E-post eksisterer ikke.", type: "email" });
+    return;
   }
+
+  if (user.password !== password) {
+    response.status(401).json({ message: "Feil passord.", type: "password" });
+    return;
+  }
+
+  response.status(200).json({ API_KEY: process.env.API_KEY });
 });
 
 /* Endepunkt: Bytt ut :code med ønsket HTTP-statuskode, og APIet sender en respons tilbake med den statuskoden. Nyttig for testing av 404, 500 og så videre. */
